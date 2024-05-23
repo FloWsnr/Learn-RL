dd13f688b2c695331c731bf33f27ce3c13d36590
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-05-23 21:50:42 +0200

    add train and infer, not bugfree yet

--- learn_rl/algo/qlearning.py
+++ learn_rl/algo/qlearning.py
@@ -2,6 +2,7 @@
 Simple Q-Learning (with Q table) algorithm
 """
 
+from pathlib import Path
 import numpy as np
 import gymnasium as gym
 
@@ -9,10 +10,18 @@ from learn_rl.environment._base import Environment
 
 
 class QLearning:
-    def __init__(self, env: Environment) -> None:
-        self.eps_0 = 1.0  # Initial epsilon for epsilon-greedy policy
-        self.alpha = 0.5  # Learning rate
-        self.gamma = 0.99  # Discount factor
+    def __init__(
+        self,
+        env: Environment,
+        alpha: float = 0.5,
+        gamma: float = 0.99,
+        eps_0: float = 1.0,
+        eps_decay: float = 0.99,
+    ) -> None:
+        self.eps_0 = eps_0  # Initial epsilon for epsilon-greedy policy
+        self.eps_decay = eps_decay  # Decay rate of epsilon
+        self.alpha = alpha  # Learning rate
+        self.gamma = gamma  # Discount factor
 
         self.num_states = env.state_space_size
         self.num_actions = env.action_space_size
@@ -31,18 +40,55 @@ class QLearning:
             return np.random.randint(self.num_actions)
         else:
             # Optimal action of that state
-            return np.argmax(self.q_table[state])
+            return np.argmax(self.q_table[state, :])
 
     def update_q_table(
         self,
         state: int,
         action: int,
-        reward: float,
         next_state: int,
+        reward: float,
     ) -> None:
         Q_s = self.q_table[state, action]
-        Q_s_next = np.max(self.q_table[next_state])
+        Q_s_next = np.max(self.q_table[next_state, :])
         td_target = reward + self.gamma * Q_s_next
         td_error = td_target - Q_s
 
         self.q_table[state, action] += self.alpha * td_error
+
+    def train(self, num_episodes: int = 1000) -> None:
+        epsilon = self.eps_0
+        for _ in range(num_episodes):
+            self.env.reset()
+            done = False
+            while not done:
+                state = self.env.observation
+                info = self.env.info
+
+                action = self.eps_greedy_policy(state, epsilon)
+                self.env.step(action)
+                next_state = self.env.observation
+                reward = self.env.reward
+                done = self.env.terminated or self.env.truncated
+
+                self.update_q_table(state, action, next_state, reward)
+                state = next_state
+
+            # Decay epsilon
+            epsilon = epsilon * self.eps_decay
+
+    def save(self, filename: Path | str) -> None:
+        np.save(filename, self.q_table)
+
+    def load(self, filename: Path | str) -> None:
+        self.q_table = np.load(filename)
+
+    def infer(self, num_episodes: int = 100) -> None:
+        for _ in range(num_episodes):
+            self.env.reset()
+            done = False
+            while not done:
+                state = self.env.observation
+                action = np.argmax(self.q_table[state, :])
+                self.env.step(action)
+                done = self.env.terminated or self.env.truncated
--- tests/test_algo/test_qlearning.py
+++ tests/test_algo/test_qlearning.py
@@ -1,13 +1,18 @@
+import numpy as np
 from learn_rl.algo.qlearning import QLearning
+from learn_rl.environment.frozenlake import FrozenLakeEnv
 
 
 def test_eps_greedy():
-    qlearning = QLearning(env=None)
-    qlearning.q_table = [
-        [0.1, 0.2, 0.3],
-        [0.4, 0.5, 0.6],
-        [0.7, 0.8, 0.9],
-    ]
+    env = FrozenLakeEnv()
+    qlearning = QLearning(env=env)
+    qlearning.q_table = np.array(
+        [
+            [0.1, 0.2, 0.3],
+            [0.4, 0.5, 0.6],
+            [0.7, 0.8, 0.9],
+        ]
+    )
 
     # Greedy action
     assert qlearning.eps_greedy_policy(0, 0.0) == 2
@@ -18,3 +23,39 @@ def test_eps_greedy():
     assert qlearning.eps_greedy_policy(0, 1.0) in [0, 1, 2]
     assert qlearning.eps_greedy_policy(1, 1.0) in [0, 1, 2]
     assert qlearning.eps_greedy_policy(2, 1.0) in [0, 1, 2]
+
+
+def test_update_q_table():
+    env = FrozenLakeEnv()
+    qlearning = QLearning(env=env, alpha=0.5, gamma=0.99)
+    qlearning.q_table = np.array(
+        [
+            [0.1, 0.2, 0.3],
+            [0.4, 0.5, 0.6],
+            [0.7, 0.8, 0.9],
+        ]
+    )
+
+    qlearning.update_q_table(state=0, action=1, next_state=1, reward=1.0)
+    # Q(0, 1) = 0.2 + 0.5 * ((1.0 + 0.99 * 0.6) - 0.2) = 0.897
+
+    assert qlearning.q_table[0, 1] == 0.897
+
+
+def test_train():
+    env = FrozenLakeEnv(render_mode=None)
+    qlearning = QLearning(env=env, alpha=0.01, gamma=0.99)
+    qlearning.train(num_episodes=1000)
+    assert True
+
+
+def test_infer():
+    env = FrozenLakeEnv()
+    qlearning = QLearning(env=env, alpha=0.001, gamma=0.99)
+    qlearning.train(num_episodes=1000)
+
+    new_env = FrozenLakeEnv(render_mode="human")
+    qlearning.env = new_env
+    qlearning.infer(num_episodes=100)
+
+    assert True
